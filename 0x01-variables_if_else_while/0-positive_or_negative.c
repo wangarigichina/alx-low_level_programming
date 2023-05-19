bf831d41639900cb20fbd3ef534b2e5a749a12a0
Wangari Gichina <ruthgichina@gmail.com>
2023-05-19 08:14:56 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,11 @@
 /* more headers goes there */
 
 /**
- * betty style doc for function main goes there
+ * main:  prints whether n is positive, zero or negative
+ * @n: random number
+ * 
  * Description: This program will assign a random number
  * to the variable n each time it is executed.
- * Complete the source code in order to print whether
- * the number stored in the variable n
- * is positive or negative
  * Return: 0
  */
 int main(void)
