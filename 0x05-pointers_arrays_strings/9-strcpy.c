20a6883eaadd47991802c19397bfb974c5dd0f25
Wangari Gichina <ruthgichina@gmail.com>
2023-05-30 18:35:01 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string src to buffer pointed to by dest
+ * @dest: string pointer
+ * @src: buffer pointer
+ * Description: cpies string src to buffer
+ * Return: dest pointer
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	int a  = 0;
+
+	while (a >= 0)
+	{
+		dest[a] = src[a];
+
+		if (str[a] == '\0')
+			return (dest);
+		i++;
+	}
+	return (dest);
+}
