ad755cf3e29afcb95619a3028f737599cd281085
Wangari Gichina <ruthgichina@gmail.com>
2023-05-30 17:43:06 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - writes every other character
+ * @str: input string to print
+ * Description: prints every other character of string
+ * Return: nothing
+ **/
+
+ void puts2(char *str)
+ {
+	 int a = 0;
+	 int b = 0;
+
+	 while (str[a] != '\0')
+	 {
+		 a++;
+	 }
+
+	 while (b < a)
+	 {
+		 _putchar(str[b]);
+		 b = b + 2;
+	 }
+
+	 _putchar('\n');
+ }
