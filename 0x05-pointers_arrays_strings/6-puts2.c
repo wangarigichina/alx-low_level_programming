24045835e81ad438fc604c96d8614e50a9d6fc3b
Wangari Gichina <ruthgichina@gmail.com>
2023-05-30 17:46:25 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,21 +7,21 @@
  * Return: nothing
  **/
 
- void puts2(char *str)
- {
-	 int a = 0;
-	 int b = 0;
+void puts2(char *str)
+{
+	int a = 0;
+	int b = 0;
 
-	 while (str[a] != '\0')
-	 {
-		 a++;
-	 }
+	while (str[a] != '\0')
+	{
+		a++;
+	}
 
-	 while (b < a)
-	 {
-		 _putchar(str[b]);
-		 b = b + 2;
-	 }
+	while (b < a)
+	{
+		_putchar(str[b]);
+		b = b + 2;
+	}
 
-	 _putchar('\n');
- }
+	_putchar('\n');
+}
