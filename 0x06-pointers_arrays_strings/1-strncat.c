cbb339e431b16cfbefb424da011ae8b05ee1b29c
Wangari Gichina <ruthgichina@gmail.com>
2023-06-02 16:28:58 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int a = 0;
-	int b;
+	int b = 0;
 
 	while (dest[a] != '\0')
 		a++;
 
-	for (b = 0; b < n; b++)
+	for (; b < n; b++)
 	{
 		dest[a] = src[b];
 		a++;
