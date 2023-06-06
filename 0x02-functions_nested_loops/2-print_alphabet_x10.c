f7e9a143d9fe0bfcb35eca1a966e2b40bb47ab1b
Wangari Gichina <ruthgichina@gmail.com>
2023-06-06 13:25:53 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints lower case alphabet
+ * print_alphabet_x10 - function to print abc 10 times
  *
- **/
+ * Return: 0
+ */
 
 void print_alphabet(void)
 {
