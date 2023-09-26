4058097c00f3f4577cde81cf2e7ff4e95d288c53
Wangari Gichina <ruthgichina@gmail.com>
2023-09-26 02:17:57 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
