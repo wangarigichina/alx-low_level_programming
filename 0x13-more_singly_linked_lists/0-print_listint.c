03ad191fe4891fddd9b8ac2562c6018c5c71e751
Wangari Gichina <ruthgichina@gmail.com>
2023-10-24 08:31:04 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i;
+	size_t nodes = 0;
 
-	for (i = 0; h; i++)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		nodes++;
 	}
-	return (i);
+	return (nodes);
 }
