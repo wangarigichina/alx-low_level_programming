4a1e9473f78e43c123eafab0830ca95dae8feb74
Wangari Gichina <ruthgichina@gmail.com>
2023-10-25 11:05:32 -0700

    10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			i++;
 		}
 		dnode = temp;
-		dnode =dnode->next;
+		dnode = dnode->next;
 		temp->next = dnode->next;
 		free(dnode);
 	}
