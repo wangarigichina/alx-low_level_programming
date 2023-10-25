4b6c08ecb1ff75fed760e74c7dc17d9ee6f6073a
Wangari Gichina <ruthgichina@gmail.com>
2023-10-25 10:57:27 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,7 @@
  * @head: head of the list
  * @idx: place to insert node
  * @n: value of the inserted node
- * Return: pointer to head of list
+ * Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
